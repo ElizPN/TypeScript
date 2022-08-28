56bb658adb70c375ebafd9f472a1fe0bf4b94896
ElizPN <pankevych.liza@gmail.com>
2022-08-28 16:58:45 +0200

    check type of parametr by hand

---
+++ type_check.js
@@ -0,0 +1,29 @@
+let customersArray = [
+  "Custy Stomer",
+  "C. Oostomar",
+  "C.U.S. Tomer",
+  3432434,
+  "Custo Mer",
+  "Custopher Ustomer",
+  3432435,
+  "Kasti Yastimeur",
+];
+
+function checkCustomersArray(arr) {
+  for (let el of arr) {
+    if (typeof el !== "string") {
+      console.log(`Type error: ${el} should be a string!`);
+    }
+  }
+}
+
+checkCustomersArray(customersArray);
+
+function stringPush(val) {
+  if (typeof val === "string") {
+    customersArray.push(val);
+  }
+  return customersArray;
+}
+let res = stringPush("hallo");
+console.log(res);
