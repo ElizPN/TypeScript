9682a81eb6c0248ac6990db9957b7977e090ca68
ElizPN <pankevych.liza@gmail.com>
2022-08-28 16:49:34 +0200

    rest parametr

---
+++ rest_param.js
@@ -0,0 +1,12 @@
+function addPower(p) {
+    var numsToAdd = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        numsToAdd[_i - 1] = arguments[_i];
+    }
+    var answer = 0;
+    for (var i = 0; i < numsToAdd.length; i++) {
+        answer += Math.pow(numsToAdd[i], p);
+    }
+    return answer;
+}
+addPower(2, 3, 4);
