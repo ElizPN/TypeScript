23f4fc202abb3b14925ba3a144cec627d5438936
ElizPN <pankevych.liza@gmail.com>
2022-09-01 12:49:02 +0200

    extends interface

---
+++ deep_types.ts
@@ -0,0 +1,30 @@
+interface Directory2 {
+  addFile: (name: string) => void;
+  config: {
+    default: {
+      encoding: string;
+      permissions: string;
+    };
+  };
+}
+
+class DesktopDirectory2 implements Directory2 {
+  config = {
+    default: {
+      encoding: "utf-8",
+      permissions: "drw-rw-rw-",
+    },
+  };
+
+  addFile(name: string) {
+    console.log(`Adding file: ${name}`);
+  }
+
+  showPreview(name: string) {
+    console.log(`Opening preview of file: ${name}`);
+  }
+}
+
+const Desktop2 = new DesktopDirectory2();
+
+console.log(Desktop2.config);
